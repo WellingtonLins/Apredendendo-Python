93de8325d42abdbf9096677a9266de2d33f254c0
wellingtonlins <wellingtonlins2013@gmail.com>
2019-01-19 20:03:36 -0300

    Somando Numeros

---
+++ LendoNumerosESomando.py
@@ -0,0 +1,6 @@
+numero1= input("Digite o primeiro numero = ");
+numero2= input("Digite o segundo numero = ");
+
+soma = numero1 + numero2;
+print("\nA resposta eh:")
+print(soma);
---
+++ Soma.py
@@ -0,0 +1,2 @@
+resposta = 2 + 5;
+print("2 + 5 =  {}" .format(resposta));
