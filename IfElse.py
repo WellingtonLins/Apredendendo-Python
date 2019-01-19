0bd370583afadb314134cd5dfef1cf01ca668cf5
wellingtonlins <wellingtonlins2013@gmail.com>
2019-01-19 20:31:16 -0300

    Comandos If e Else

---
+++ IfElse.py
@@ -0,0 +1,5 @@
+x = input('Please enter an integer: ')
+if x < 0:
+	print 'Negative number!'   
+else:
+	print 'Positive number!'
---
+++ idade.py
@@ -0,0 +1,5 @@
+idade = input("Digite sua idade")
+if idade <= 18: 
+	print'Voce tem MENOS de 18 anos de idade :('
+if idade >= 18:
+	print'Voce tem MAIS de 18 anos de idade :)'
