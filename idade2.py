9c9e0eaaa0314c98d5bd39abfd432a6a6f363960
wellingtonlins <wellingtonlins2013@gmail.com>
2019-01-21 14:48:53 -0300

    Aprendendo o comando de repeticao While

--- idade2.py
+++ idade2.py
@@ -2,6 +2,6 @@ idade = input("Digite sua idade")
 if idade < 18: 
 	print'Voce tem MENOS de 18 anos de idade :('
 elif idade < 60:
-		print'Voce tem MAIS de 18 anos de idade :)'
+	print'Voce tem MAIS de 18 anos de idade :)'
 else:
 	print'Voce tem MAIS de 60 anos de idade :)'
---
+++ while2.py
@@ -0,0 +1,5 @@
+contador = 0
+
+while(contador < 5):
+	print('Contador valor atual =  {}').format(contador);
+	contador += 1
