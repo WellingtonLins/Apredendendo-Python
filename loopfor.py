1685d3bd24867d499534469c49668ca4a6cdbbdf
wellingtonlins <wellingtonlins2013@gmail.com>
2019-01-21 22:36:53 -0300

    O loop for

---
+++ loopfor.py
@@ -0,0 +1,15 @@
+print 'Precorrendo o array de letras\n'
+
+for letra in 'Java': #first Example
+	print 'Letra corrente => ', letra
+
+frutas = ['banana','mamao','goiaba'] #declarando o  ARRAY
+
+print '\nPrecorrendo o array de frutas\n'
+
+for fruta  in frutas:
+   print 'fruta', fruta
+
+
+print 'FIM'
+
