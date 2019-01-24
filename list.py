4f9cd7f91b10eaaae8359869f0326d87f552252b
wellingtonlins <wellingtonlins2013@gmail.com>
2019-01-23 22:58:27 -0300

    Atribuindo novos valores aos elementos da lista

--- list.py
+++ list.py
@@ -1,10 +1,8 @@
 frutas = ['maca','banana','pera','melancia','jaca','melao']
 
 print 'frutas => ', frutas
-print 'fruta[0] => ', frutas[0] # o indice em python começa no 0
+print 'fruta[0] => ', frutas[0] # o indice em python comeca no 0
 print 'frutas[1] => ', frutas[1] 
 print 'frutas[5] => ', frutas[5]
 #imprime o que esta entre os indices 1 e 3 =  ['banana', 'pera']
 print 'frutas[1:5] => ', frutas[1:3]
-
-
---
+++ list2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+list = ['Fisica', 'quimica', 1997, 2000];
+
+print "Lista inicial"
+print list
+print "Valor do disponivel do indice 2 : "
+print list[2]
+list[2] = 2001;
+print "Novo valor disponivel do indice 2 : "
+print list[2]
+print "Lista final"
+print list
