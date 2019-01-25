d03344b558a9e54d272194613697a090df9497ac
wellingtonlins <wellingtonlins2013@gmail.com>
2019-01-24 23:18:12 -0300

    Utilizando dicionario em python

---
+++ dicionario-update.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print 'Dicionario inicial'
+print dict
+
+dict['Age'] = 8; # atualizando uma entrada existente
+dict['School'] = "DPS School"; # Adicionando nova entrada
+
+print "dict['Age']: ", dict['Age']
+print "dict['School']: ", dict['School']
+
+print 'Dicionario final'
+print dict
